d7269cdaeb75895ae4572df9088536346f8b6c09
Tiep Tran <51280433+polskiTran@users.noreply.github.com>
2026-04-20 22:33:26 -0400

    Update markdown_builder.py

--- src/rec2note_cli/utils/markdown_builder.py
+++ src/rec2note_cli/utils/markdown_builder.py
@@ -20,23 +20,23 @@ def build_markdown(
 
     # Summary
     markdown += "## Summary\n\n"
-    markdown += f"### Overview\n{summary.overview}\n\n"
+    markdown += f"### Overview\n > [!NOTE]\n > {summary.overview}\n\n"
     markdown += "### Key Points\n"
     for key_point in summary.key_points:
         markdown += f"- {key_point}\n"
     markdown += "### Topics\n"
     for topic in summary.topics:
-        markdown += f"- [{topic.topic}] {topic.details}\n"
+        markdown += f"- **{topic.topic}** {topic.details}\n"
     markdown += "### Key Terms\n"
     for key_term in summary.key_terms:
-        markdown += f"- [{key_term.term}] {key_term.definition}\n"
+        markdown += f"- **{key_term.term}** {key_term.definition}\n"
 
     # Student Questions
     if student_qa:
         markdown += "## Student Questions\n"
         for question in student_qa:
-            markdown += f"- Q: ({question.question_timestamp}) {question.question}\n"
-            markdown += f"    - A: {question.answer}\n\n"
+            markdown += f"> Q: ({question.question_timestamp}) {question.question}\n"
+            markdown += f"- A: {question.answer}\n\n"
     # Deadline
     if deadline:
         markdown += "## Deadline\n"
@@ -47,8 +47,8 @@ def build_markdown(
     if study_qs:
         markdown += "## Study Questions\n\n"
         for study_question in study_qs:
-            markdown += f"- Q: ({study_question.timestamp_reference}) [{study_question.type}] {study_question.question}\n"
-            markdown += f"    - A: {study_question.answer}\n\n"
+            markdown += f"> Q: ({study_question.timestamp_reference}) [{study_question.type}] {study_question.question}\n"
+            markdown += f"- A: {study_question.answer}\n\n"
 
     return markdown
 
